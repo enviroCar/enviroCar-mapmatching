d1357d05f739f2979f0d92a9c6cd661ec7b1cb88
Wall, Arne de (52-North) <a.dewall@52north.org>
2019-06-17 13:48:23 +0200

    initial spring boot project

---
+++ src/main/kotlin/org/envirocar/processing/mapmatching/MapmatchingApplication.kt
@@ -0,0 +1,11 @@
+package org.envirocar.processing.mapmatching
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapmatchingApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapmatchingApplication>(*args)
+}
