cafe44783ab3ed75873ff6cb25e3ccd35745d05a
Wall, Arne de (52-North) <a.dewall@52north.org>
2019-07-09 09:10:13 +0200

    applied license header

--- src/main/kotlin/org/envirocar/processing/mapmatching/models/mapmatching/MapMatchingInput.kt
+++ src/main/kotlin/org/envirocar/processing/mapmatching/models/mapmatching/MapMatchingInput.kt
@@ -1,61 +1,23 @@
+/**
+ *
+ * Copyright (C) 2013-2019 The enviroCar project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.envirocar.processing.mapmatching.models.mapmatching
 
-import org.locationtech.jts.geom.Coordinate
-import org.locationtech.jts.geom.GeometryFactory
 
-
-
-class MapMatchingInput(
+data class MapMatchingInput(
         val candidates: List<MapMatchingCandidate>
-) {
-//    companion object {
-//        @Throws(ParseException::class)
-//        fun fromString(input: String): MapMatchingInput {
-//            val factory = GeometryFactory() // todo
-//            val result = MapMatchingInput()
-//
-//            val jsonParser = JSONParser()
-//
-//            val track = jsonParser.parse(input) as JSONObject
-//            val features = track.get('features') as JSONArray
-//
-//            features.forEach { f ->
-//                val feature = f as JSONObject
-//                val geometry = feature.get('geometry') as JSONObject
-//
-//                // Get the geometry
-//                val coordinates = geometry.get('coordinates') as JSONArray
-//                val latitude = coordinates.get(1) as Double
-//                val longitude = coordinates.get(0) as Double
-//
-//                // Get the properties
-//                val properties = feature.get('properties') as JSONObject
-//                val id = properties.get('id') as String
-//                val timeString = properties.get('time') as String
-//
-//                // Get the GPS Accuracy
-//                val phenomenons = properties.get('phenomenons') as JSONObject
-//                val gpsAccuracy = phenomenons.get('GPS Accuracy') as JSONObject
-//                val accuracy = gpsAccuracy.get('value') as Double
-//
-//                var time: Date? = null
-//                try {
-//                    time = DATE_FORMAT.parse(timeString)
-//                    val point = factory.createPoint(
-//                            Coordinate(longitude, latitude))
-//                    val e = MapMatchingCandidate(
-//                            id, time, point)
-//                    e.setAccuracy(accuracy)
-//                    result.addCandidate(e)
-//                } catch (ex: java.text.ParseException) {
-//                    Logger.getLogger(MapMatchingInput::class.java.name).log(
-//                            Level.SEVERE, null, ex)
-//                }
-//
-//
-//            }
-//
-//            return result
-//        }
-//    }
-}
+)
